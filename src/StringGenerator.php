628e0f0a8f427f3dd7c29ff48629a8938d50ff68
Danilo Silva <contact@danilocgsilva.me>
2024-07-31 12:05:02 +0000

    Converted main class to dynamic

--- src/StringGenerator.php
+++ src/StringGenerator.php
@@ -4,9 +4,24 @@ declare(strict_types=1);
 
 namespace Danilocgsilva\DataToSeedLaravel;
 
+use Config;
+
 class StringGenerator
 {
-    public static function generate(): string
+    private string $database;
+
+    public function __construct()
+    {
+        var_dump(Config::get('database.connections.mysql'));
+    }
+    
+    public function setDatabase(string $database): self
+    {
+        $this->database = $database;
+        return $this;
+    }
+    
+    public function generate(): string
     {
         return "Hello world!";
     }
