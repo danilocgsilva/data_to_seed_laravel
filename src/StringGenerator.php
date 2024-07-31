986fb324591bbb14e9ddda8b86d057ada61ddf77
Danilo Silva <contact@danilocgsilva.me>
2024-07-31 11:51:47 +0000

    First commit

---
+++ src/StringGenerator.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Danilocgsilva\DataToSeedLaravel;
+
+class StringGenerator
+{
+    public static function generate(): string
+    {
+        return "Hello world!";
+    }
+}
